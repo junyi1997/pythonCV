30212d24169ced269abcbc841dc9c9a28682cb80
junyi <q5896799@gmail.com>
2019-04-02 18:47:38 +0800

    'debug'

--- 123.py
+++ 123.py
@@ -4,7 +4,7 @@ im1 = cv2.imread('text.jpg')
 cv2.imshow('image1', im1)
 cv2.waitKey(0)
 
-im2 = cv2.resize(im1, (10,10), interpolation=cv2.INTER_CUBIC)
+im2 = cv2.resize(im1, (100,100), interpolation=cv2.INTER_CUBIC)
 cv2.imshow('image2', im2)
 cv2.waitKey(0)
 cv2.imwrite('lena2.jpg', im2)
