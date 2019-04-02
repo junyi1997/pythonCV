2853f1634987f50e5bba6cade15777f70c9a6ec6
junyi <q5896799@gmail.com>
2019-04-02 19:39:43 +0800

    'debug'

--- 1.py
+++ 1.py
@@ -1,13 +1,25 @@
-import cv2
 
-cap = cv2.VideoCapture(0)
-while(1):
-    # get a frame
-    ret, frame = cap.read()
-    # show a frame
-    cv2.imshow("capture", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        cv2.imwrite("/image/test.jpeg", frame)
-        break
-cap.release()
-cv2.destroyAllWindows()
+import cv2
+ 
+def make_photo():
+    """使用opencv拍照"""
+    cap = cv2.VideoCapture(0)  # 默认的摄像头
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            cv2.imshow("capture", frame)  # 弹窗口
+            # 等待按键q操作关闭摄像头
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                file_name = "test1.jpeg"
+                cv2.imwrite(file_name, frame)
+                break
+        else:
+            break
+ 
+    cap.release()
+    cv2.destroyAllWindows()
+ 
+ 
+if __name__ == '__main__':
+    make_photo()
+
