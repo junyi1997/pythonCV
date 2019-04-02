2b30f80337d39b6a1da848dc2958b9d99e9e13d7
junyi <q5896799@gmail.com>
2019-04-02 18:50:47 +0800

    'debug'

--- 123.py
+++ 123.py
@@ -1,10 +1,17 @@
+
 import cv2
+import numpy as np
+ 
+img = cv2.imread('test.jpg')
+resImg1 = cv2.resize(img, (300,300), interpolation=cv2.INTER_CUBIC)
 
-im1 = cv2.imread('text.jpg')
-cv2.imshow('image1', im1)
-cv2.waitKey(0)
+resImg2 = cv2.resize(img,(img.shape[1],img.shape[0]), interpolation=cv2.INTER_CUBIC)
+img1 = cv2.resize(img, None, fx=0.1,fy=0.5,interpolation=cv2.INTER_CUBIC)
 
-im2 = cv2.resize(im1, (100,100), interpolation=cv2.INTER_CUBIC)
-cv2.imshow('image2', im2)
-cv2.waitKey(0)
-cv2.imwrite('lena2.jpg', im2)
+cv2.imshow('img', img)
+cv2.imshow('resImg', resImg1)
+cv2.imshow('resImg2', resImg2)
+cv2.imshow('img1', img1)
+cv2.waitKey()
+ 
+cv2.destroyAllWindows()
