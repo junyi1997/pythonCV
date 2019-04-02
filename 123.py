6505015a30050279b9c72421df5c9eb40ce2d066
junyi <q5896799@gmail.com>
2019-04-02 18:55:05 +0800

    'debug'

--- 123.py
+++ 123.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
  
 img = cv2.imread('test.jpg')
-resImg1 = cv2.resize(img, (300,300), interpolation=cv2.INTER_CUBIC)
+resImg1 = cv2.resize(img, (100,100), interpolation=cv2.INTER_CUBIC)
 
 cv2.imshow('img', img)
 cv2.imshow('resImg', resImg1)
