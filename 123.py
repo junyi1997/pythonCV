0e2fe9e1cdde316a79b0e569894f7422d43eb7f9
junyi <q5896799@gmail.com>
2019-04-02 18:42:01 +0800

    'debug'

--- 123.py
+++ 123.py
@@ -1,7 +1,10 @@
 import cv2
-image=cv2.imread('test.jpg') 
-res=cv2.resize(image,(32,32),interpolation=cv2.INTER_CUBIC) 
-cv2.imshow('iker',res) 
-cv2.imshow('image', image) 
-cv2.waitKey(0) 
-cv2.destoryAllWindows()
+
+im1 = cv2.imread('lena.jpg')
+cv2.imshow('image1', im1)
+cv2.waitKey(0)
+
+im2 = cv2.resize(im1, (10,10), interpolation=cv2.INTER_CUBIC)
+cv2.imshow('image2', im2)
+cv2.waitKey(0)
+cv2.imwrite('lena2.jpg', im2)
