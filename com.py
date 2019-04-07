b29c8adb0f234480a3529ce40118a8a1cd598f3b
junyi <q5896799@gmail.com>
2019-04-07 11:17:17 +0800

    'debug'

--- com.py
+++ com.py
@@ -20,16 +20,17 @@ def make_photo():
         if ret:
             cv2.imshow("capture", frame)  # 弹窗口
             # 等待按键q操作关闭摄像头12313
-            '''
+            
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 file_name = "test.jpg"
                 cv2.imwrite(file_name, frame)
 
                 break
-            '''    
+               
             file_name = "test1.jpg"
             cv2.imwrite(file_name, frame)  
-            break  
+            break 
+            ''' 
         else:
             break
     adj(file_name)
