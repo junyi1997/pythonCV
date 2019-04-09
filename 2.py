9a9521635e5b37b30758dbb58c615e1a473c1038
JohnnieWu <q5896799@gmail.com>
2019-04-09 14:14:31 +0800

    'first_commit'

--- 2.py
+++ 2.py
@@ -7,7 +7,7 @@ import numpy as np
 import time
 sum1=1
 
-
+'''
 def adj(c):
     print("影像處理中")
     img = cv2.imread(c)
@@ -18,7 +18,7 @@ def adj(c):
     #cv2.imshow('resImg', resImg1)
     #cv2.waitKey()
     p(c)
-
+'''
 
 def make_photo(c):
     """使用opencv拍照"""
@@ -27,9 +27,15 @@ def make_photo(c):
         ret, frame = cap.read() # 從攝影機擷取一張影像
         if ret:
             cv2.imshow("capture", frame)  # 顯示圖片
-            
-            
-            
+            print("影像處理中")
+            img = cv2.imread(c)
+            resImg1 = cv2.resize(img, (100,100), interpolation=cv2.INTER_CUBIC)
+            print("影像顯示")
+            cv2.imwrite(c,resImg1)
+            #cv2.imshow('img', img)
+            #cv2.imshow('resImg', resImg1)
+            #cv2.waitKey()
+            p(c)            
             # 若按下 q 鍵則離開迴圈
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 #file_name =c
@@ -45,7 +51,6 @@ def make_photo(c):
         else:
             print("沒有影像")
             break
-    adj(c)
     # 釋放攝影機
     cap.release()
     # 關閉所有 OpenCV 視窗
