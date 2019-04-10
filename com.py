b525a58cc8a65b9890d900a24f69167baf53742b
JohnnieWu <q5896799@gmail.com>
2019-04-10 08:37:36 +0800

    'first_commit'

--- com.py
+++ com.py
@@ -3,9 +3,9 @@ import cv2
 import numpy as np
 import time
 sum1=1
-def adj(file_name):
+def adj(c):
     print("影像處理中")
-    img = cv2.imread(file_name)
+    img = cv2.imread(c)
     resImg1 = cv2.resize(img, (100,100), interpolation=cv2.INTER_CUBIC)
 
     #cv2.imshow('img', img)
@@ -13,7 +13,7 @@ def adj(file_name):
     cv2.waitKey()
 
 
-def make_photo():
+def make_photo(sum1):
     """使用opencv拍照"""
     cap = cv2.VideoCapture(0)  # 默认的摄像头
     while True:
@@ -30,17 +30,17 @@ def make_photo():
             '''   
             time.sleep(1)
             c="test"+str(sum1)+".jpg"
-            file_name =c
-            cv2.imwrite(file_name, frame)  
+            
+            cv2.imwrite(c, frame)  
             break 
             
         else:
             break
-    adj(file_name)
+    adj(c)
     cap.release()
     cv2.destroyAllWindows()
  
 if __name__ == '__main__':
     sum1=input("請輸入sum1值：")
-    make_photo()
+    make_photo(sum1)
 
