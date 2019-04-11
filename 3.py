50134a2b96d1840d78f015f1c387f087ccd2eee5
JohnnieWu <q5896799@gmail.com>
2019-04-11 09:41:03 +0800

    '簡化測試'

--- 3.py
+++ 3.py
@@ -4,7 +4,7 @@ import tkinter as tk
 import cv2
 import numpy as np
 import time
-'''  
+   
 def p(c):
     print("進入Tk頁面")
     win = tk.Tk()
@@ -24,14 +24,15 @@ def adj(c):
     cv2.imshow('resImg', resImg1)
     #cv2.waitKey()
     p(c)
-'''
+
 
 def make_photo():
-    sum1=input("請輸入sum1值：")
     while True:
+        sum1=input("請輸入sum1值：")
         if sum1=='q':
             break
         else:
+            c="test"+str(sum1)+".jpg"
             """使用opencv拍照"""
             cap = cv2.VideoCapture(0)  # 默认的摄像头
             while True:
@@ -39,27 +40,12 @@ def make_photo():
                 ret, frame = cap.read()
                 if ret:
                     time.sleep(1)
-                    c="test"+str(sum1)+".jpg"
                     cv2.imwrite(c, frame)  
-                    print("影像處理中")
-                    img = cv2.imread(c)
-                    resImg1 = cv2.resize(img, (100,100), interpolation=cv2.INTER_CUBIC)
-                    #cv2.imshow('img', img)
-                    cv2.imwrite(c, resImg1) 
-                    cv2.imshow('resImg', resImg1)
-                    #cv2.waitKey()
-                    print("進入Tk頁面")
-                    win = tk.Tk()
-                    #im=Image.open(c)
-                    img=ImageTk.PhotoImage(Image.open(c))
-                    #img=tk.PhotoImage(file=r"./"+c)
-                    imLabel=tk.Label(win,image=img)
-                    imLabel.place(x=290,y=213)
-                    win.mainloop()
                     break 
                 else:
                     print("拍照失敗")
                     break
+            adj(c)
             cap.release()
             cv2.destroyAllWindows()
  
